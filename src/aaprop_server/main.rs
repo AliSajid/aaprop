4fe615be829632765accb11cb3abbf83c597006b
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-03-29 23:47:00 -0400

    feat(aaprop-server): have a functioning standalone binary
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/aaprop_server/main.rs
+++ src/aaprop_server/main.rs
@@ -3,6 +3,38 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-fn main() {
-    println!("This is a server.");
+mod cli;
+
+use aaprop::create_router;
+use clap::Parser;
+use cli::Cli;
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    let bind_addr = format!("{}:{}", cli.bind, cli.port);
+
+    let router = create_router();
+
+    let listener = match tokio::net::TcpListener::bind(&bind_addr).await {
+        Ok(listener) => {
+            println!("Server started at http://{}", &bind_addr);
+            listener
+        }
+        Err(e) => {
+            eprintln!("Failed to start server: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    match axum::serve(listener, router).await {
+        Ok(_) => {
+            println!("Listening on: http://{}", &bind_addr);
+        }
+        Err(e) => {
+            eprintln!("Server error: {}", e);
+            std::process::exit(2);
+        }
+    }
 }
