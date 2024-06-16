19ddfcddf736e37d598df799ac992c658c7d6e12
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-06-15 21:19:09 -0400

    test(router): add tests for the router functions
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/aaprop_lib/lib.rs
+++ src/aaprop_lib/lib.rs
@@ -64,3 +64,58 @@ fn create_router_v1() -> Router {
 pub fn create_router() -> Router {
     Router::new().nest("/v1", create_router_v1())
 }
+
+#[cfg(test)]
+mod tests {
+    use axum_test::TestServer;
+    use rstest::{
+        fixture,
+        rstest,
+    };
+    use shuttle_runtime::tokio;
+
+    use super::*;
+
+    #[fixture]
+    fn test_server_with_versioning() -> TestServer {
+        TestServer::new(create_router()).unwrap()
+    }
+
+    #[fixture]
+    fn test_server_without_versioning() -> TestServer {
+        TestServer::new(create_router_v1()).unwrap()
+    }
+    #[rstest]
+    #[case::root("/")]
+    #[case::amino_acid("/amino_acid")]
+    #[case::amino_acid_name("/amino_acid/Alanine")]
+    #[case::amino_acid_short_name("/amino_acid/Ala")]
+    #[case::amino_acid_abbreviation_name("/amino_acid/A")]
+    #[case::amino_acid_side_chain("/amino_acid/Alanine/side_chain")]
+    #[case::amino_acid_molecular_weight("/amino_acid/Alanine/molecular_weight")]
+    #[case::amino_acid_codons("/amino_acid/Alanine/codon")]
+    #[case::amino_acid_codon_count("/amino_acid/Alanine/codon_count")]
+    #[tokio::test]
+    async fn test_create_router(test_server_without_versioning: TestServer, #[case] url: &str) {
+        let response = test_server_without_versioning.get(url).await;
+        assert_eq!(response.status_code(), StatusCode::OK);
+    }
+
+    #[rstest]
+    #[case::amino_acid("/v1/amino_acid")]
+    #[case::amino_acid_name("/v1/amino_acid/Alanine")]
+    #[case::amino_acid_short_name("/v1/amino_acid/Ala")]
+    #[case::amino_acid_abbreviation_name("/v1/amino_acid/A")]
+    #[case::amino_acid_side_chain("/v1/amino_acid/Alanine/side_chain")]
+    #[case::amino_acid_molecular_weight("/v1/amino_acid/Alanine/molecular_weight")]
+    #[case::amino_acid_codons("/v1/amino_acid/Alanine/codon")]
+    #[case::amino_acid_codon_count("/v1/amino_acid/Alanine/codon_count")]
+    #[tokio::test]
+    async fn test_create_router_with_version(
+        test_server_with_versioning: TestServer,
+        #[case] url: &str,
+    ) {
+        let response = test_server_with_versioning.get(url).await;
+        assert_eq!(response.status_code(), StatusCode::OK);
+    }
+}
