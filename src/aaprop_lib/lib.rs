172c51895d748f860801dc33d13f48a9539e49fb
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-02-19 01:01:37 -0500

    fix: correct the param syntax for new axum version

--- src/aaprop_lib/lib.rs
+++ src/aaprop_lib/lib.rs
@@ -29,33 +29,33 @@ fn create_router_v1() -> Router {
     Router::new()
         .route("/", get(routes::get_root))
         .route("/amino_acid", get(routes::get_root))
-        .route("/amino_acid/:amino_acid", get(routes::get_amino_acid))
+        .route("/amino_acid/{amino_acid}", get(routes::get_amino_acid))
         .route(
-            "/amino_acid/:amino_acid/name",
+            "/amino_acid/{amino_acid}/name",
             get(routes::get_amino_acid_name),
         )
         .route(
-            "/amino_acid/:amino_acid/short_name",
+            "/amino_acid/{amino_acid}/short_name",
             get(routes::get_amino_acid_short_name),
         )
         .route(
-            "/amino_acid/:amino_acid/abbreviation",
+            "/amino_acid/{amino_acid}/abbreviation",
             get(routes::get_amino_acid_abbreviation),
         )
         .route(
-            "/amino_acid/:amino_acid/side_chain",
+            "/amino_acid/{amino_acid}/side_chain",
             get(routes::get_amino_acid_side_chain),
         )
         .route(
-            "/amino_acid/:amino_acid/molecular_weight",
+            "/amino_acid/{amino_acid}/molecular_weight",
             get(routes::get_amino_acid_molecular_weight),
         )
         .route(
-            "/amino_acid/:amino_acid/codon",
+            "/amino_acid/{amino_acid}/codon",
             get(routes::get_amino_acid_codons),
         )
         .route(
-            "/amino_acid/:amino_acid/codon_count",
+            "/amino_acid/{amino_acid}/codon_count",
             get(routes::get_amino_acid_codon_count),
         )
         .route("/health", get(|| async { StatusCode::OK }))
